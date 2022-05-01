4124c4402b3941faec9c38828e0fb9197f801a45
Dagen Gilbreath <70980537+DagenGilbreath@users.noreply.github.com>
2022-05-01 18:31:23 -0400

    rewritten to work with websockets

--- handler.py
+++ handler.py
@@ -1,22 +1,11 @@
-import socketserver
 import sys
+import websockets
+import asyncio
 
-class MyTCPHandler(socketserver.StreamRequestHandler):
-    def __init__(self, ip, port, server_id):
-        self.ip = ip
-        self.port = port
-        self.id = server_id
-        self.open = True
-    
-    def handle(self):
-        # self.rfile is a file-like object created by the handler;
-        # we can now use e.g. readline() instead of raw recv() calls
-        self.data = self.rfile.readline().strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # Likewise, self.wfile is a file-like object used to write back
-        # to the client
-        self.wfile.write(self.data.upper())
+async def produce(message: str, host: str, port: int) -> None:
+    async with websockets.connect(f"ws://{host}:{port}") as ws:
+        await ws.send(message)
+        await ws.recv()
 
 if __name__ == "__main__":
     ip = ''
@@ -26,12 +15,6 @@ if __name__ == "__main__":
             ip = arg
         elif i == 2:
             port = int(arg)
-        elif i == 3:
-            AWS_ACCESS_KEY = arg
-        elif i == 4:
-            AWS_SECRET_ACCESS_KEY = arg
 
-    aServer = socketserver.TCPServer((ip, port), MyTCPHandler)
-
-    # Listen forever
-    aServer.serve_forever()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(produce(message = 'connected!', host = ip, port = port))
